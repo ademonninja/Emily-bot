80548256de9199278983830cb6eef91247a09d36
Ashlyn Black <howtheduck@gmail.com>
2024-06-13 12:54:54 -0400

    Giving her life

--- EmilyBot.py
+++ EmilyBot.py
@@ -1,9 +1,22 @@
 # Header bullshit
-import discord
+import hikari
+import lightbulb
+import dotenv
 import os
+import asyncio
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Keeps my code sanitized for people looking at it to keep my token secret.
 KEY = os.getenv('DISCORD_TOKEN')
+
+bot = lightbulb.BotApp(token=KEY, prefix='$')
+
+@bot.command
+@lightbulb.command("ping", "checks that the bot is alive.")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx: lightbulb.Context) -> None:
+    await ctx.respond("Pong.")
+
+bot.run()
